6803fb6d2ce156aa03590fb08c06c96ef711484e
Nehal Patel <PR0Grammar@users.noreply.github.com>
2018-10-19 17:29:02 -0400

    Update. Still unfinished.

--- JS/Graphs/Trees/BinarySearchTree.js
+++ JS/Graphs/Trees/BinarySearchTree.js
@@ -1,5 +1,3 @@
-//TODO:
-
 class BinarySearchTree {
   constructor(){
     this.root = null;
@@ -13,15 +11,96 @@ class BinarySearchTree {
       this.insertNode(this.root, node); //Starting from root, find where it belongs
     
   }
-    // remove(data) 
-                  
   
+  insertNode(parentNode, newNode){
+  	if(newNode.getVal() < parentNode.getVal()){
+    	if(parentNode.getLeft() == null){
+      	parentNode.setLeft(newNode.getVal());
+      }else{
+      	this.insertNode(parentNode.getLeft(), newNode)
+      }
+    }else{
+    	if(parentNode.getRight() == null){
+      	parentNode.setRight(newNode.getVal());
+      }else{
+      	this.insertNode(parentNode.getRight(), newNode);
+      }
+    }
+  }
+    // remove(data) 
+  remove(data){
+  	this.root = this.removeData(this.root, data);
+  }
+  
+  removeNode(node, key){
+  	if(node == null)
+    	return null;
+    
+    //Search left or right depending on value
+    if(key < node.getVal()){
+    	node.left = this.removeNode(node.left, key);
+      return node;
+    }
+    else if(key > node.getVal()){
+    	node.right = this.removeNode(node.right, key);
+      return node;
+    }   
+    //Once found
+    else{
+    	if(this.isLeaf(node)){
+      	node = null;
+        return node;
+      }
+      else if(node.getLeft() == null){
+      	node = node.getRight();
+        return node;
+      }
+      else if(node.getRight() == null){
+      	node = node.getLeft();
+        return node;
+      }
+      
+      //If node has two children, we replace it with the smallest value of its right
+      //subtree and remove that smallest right node
+      let smallestNode = this.findMinNode(node.getRight());
+      node.setData(smallestNode.getData());
+      
+      node.setRight(this.removeNode(node.getRight(), smallestNode.getData()));
+    }
+  }
+  
+  isLeaf(node){
+  	return node.getRight() == null && node.getLeft() == null;
+  }
+  
+  inOrderPrint(node){
+  	if(node == null)
+    	return;
+    this.inOrderPrint(node.getLeft());
+    console.log(node.getVal());
+    this.inOrderPrint(node.getRight());
+  }
+  
+  preOrderPrint(node){
+  	if(node == null)
+    	return;
+    console.log(node.getVal());
+    this.preOrdePrint(node.getLeft());
+    this.preOrderPrint(node.getRight());
+  }
+  
+  postOrderPrint(node){
+  	if(node == null)
+    	return;
+    this.postOrderPrint(node.getLeft());
+    this.postOrderPrint(node.getRight());
+    console.log(node.getVal());
+  }
+  
+  getRoot(){
+  	return this.root;
+  }
     // Helper function 
     // findMinNode() 
-    // getRootNode() 
-    // inorder(node) 
-    // preorder(node)                
-    // postorder(node) 
     // search(node, data) 
 }
-
