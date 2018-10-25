c52dfe091d97625f0208eff64319d5fbcca43958
Nehal Patel <PR0Grammar@users.noreply.github.com>
2018-10-24 23:33:38 -0400

    Undo most methods BST.

--- JS/Graphs/Trees/BinarySearchTree.js
+++ JS/Graphs/Trees/BinarySearchTree.js
@@ -3,25 +3,6 @@ class BinarySearchTree {
     this.root = null;
   }
   
-  insert(val){
-    if(this.root == null)
-      this.root = new BinaryNode(val);
-    else
-      this._insertHelper(this.root, val)
-  }
-  
-  findMin(node){
-    if(isLeaf(node))
-      return node.getData();
-    findMin(node.getLeft());
-  }
-  
-  findMax(node){
-    if(isLeaf(node))
-      return node.getData();
-    findMax(node.getRight());
-  }
-  
   getRoot(){
     return this.root;
   }
@@ -33,41 +14,4 @@ class BinarySearchTree {
   isInternal(node){
     return node.getLeft() != null || node.getRight() != null;
   }
-  _insertHelper(parent, val){
-    if(parent == null)
-      parent = new BinaryNode(val);
-    
-    else{
-      if(val < parent.getData()){
-        if(parent.getLeft() == null)
-          parent.setLeft(new BinaryNode(val));
-        else
-          this._insertHelper(parent.getLeft(), val);
-      }
-      
-      else{
-        if(parent.getRight() == null)
-          parent.setRight(new BinaryNode(val));
-        else
-          this._insertHelper(parent.getRight(), val);
-      }
-    }
-  }
-  
-  inOrder(node = this.root){
-    if(node == null)
-      return;
-    this.inOrder(node.getLeft());
-    console.log(node.getData());
-    this.inOrder(node.getRight());
-  }
 }
-
-
-let x = new BinarySearchTree();
-
-x.insert(4);
-x.insert(6);
-x.insert(5);
-x.insert(2);
-x.insert(8);
